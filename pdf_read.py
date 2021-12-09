5c97b8d4037c7539f4017dc86874e0a4da8e05e4
kim130727 <kim130727@gmail.com>
2021-12-09 10:28:19 +0900

    Create pdf_read.py

---
+++ pdf_read.py
@@ -0,0 +1,34 @@
+﻿#pdf 한글 추출 이게 잘됨
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
+
+
+def convert_pdf_to_txt(path):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = open(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos = set()
+
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching,
+                                  check_extractable=False):
+        interpreter.process_page(page)
+        text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
+
+
+extracted_text = convert_pdf_to_txt("D:\#.Secure Work Folder\\2021년 업무\신규유망 아이템\\수출촉진을위한농기자재산업활성화방안.pdf")
+print(extracted_text)
